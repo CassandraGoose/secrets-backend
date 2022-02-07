21ddb3a51b4a4210ed2b17f395e3c8a7ab2ddf89
cassandraGoose <cassandra.torske@gmail.com>
2022-02-07 09:05:16 -0700

    update cors config

--- app.js
+++ app.js
@@ -12,12 +12,8 @@ if (process.env.NODE_ENV !== 'test') {
 
 app.use(bodyParser.json());
 
-app.options('*', cors({
-  origin: 'https://secret-keeper.surge.sh/'
-}))
-
 app.use(cors({
-  origin: 'https://secret-keeper.surge.sh/'
+  origin: 'https://secret-keeper.surge.sh'
 }))
 
 app.use('/secrets', require('./routes/secrets'));
